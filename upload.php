2c3411b656dbdea2b4c4273c37ad63e891b08854
Olena Likhytska <olenalikhytska@gmail.com>
2015-05-13 17:59:53 +0300

    second commit

--- upload.php
+++ upload.php
@@ -1,20 +1,23 @@
-<html> 
+<html>
 <head>
-<meta charset="utf-8"> 
-<title> Результат загрузки файла </title> 
-</head> <body> 
-<?php   
-if(copy($_FILES['filename']['tmp_name'],      
-		"temp/".$_FILES['filename']['name']))   
-{     
-	echo "Файл успешно загружен";   
-}   
-else   
-{    
-	echo "Ошибка загрузки файла";   
-} 
- 
-?> 
-</body> 
-</html> 
+<meta charset="utf-8">
+<title>Результат завантаження файла</title>
+</head>
+<body>
 
+<?php
+   if($_FILES["filename"]["size"] > 1024*3*1024)
+   {
+     echo ("Розмір файлу перевищує три мегабайти");
+     exit;
+   }
+	if(is_uploaded_file($_FILES["filename"]["tmp_name"]))
+   {
+    echo("Файл успішно завантажений");
+		move_uploaded_file($_FILES["filename"]["tmp_name"], "upload/".$_FILES["filename"]["name"]);
+   } else {
+      echo("Помилка завантаженняґ файла");
+   }
+?>
+</body>
+</html> 
